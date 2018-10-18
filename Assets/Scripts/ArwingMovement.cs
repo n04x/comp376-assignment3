571795713d1a8975c6700fcef0cbadebb4ecb164
noax <thomasbacks@gmail.com>
2018-10-18 18:53:47 -0400

    create Arwing script

--- Assets/Scripts/ArwingMovement.cs
+++ Assets/Scripts/ArwingMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ArwingMovement : MonoBehaviour
 {
+    public float speed;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, vertical, 0.0f);
+        transform.Translate(movement * speed);
     }
 }
